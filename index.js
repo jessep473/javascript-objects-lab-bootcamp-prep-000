556939aad7de0f70e580105d8c3f0b1049ad2791
Jesse Peterson <jessep473@gmail.com>
2018-10-06 16:16:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ function updateObjectWithKeyAndValue(obj, key, value) {
   return Object.assign({}, obj, { [key]: value })
 }
 
-recipie = {eggs: 2 };
+const recipie = {eggs: 2 };
 
 updateObjectWithKeyAndValue(recipe, 'chocolate', '1 cup')
