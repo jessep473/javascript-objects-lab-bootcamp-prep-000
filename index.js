dcf719f2a45ee05308b50796edefc6aeef02a661
Jesse Peterson <jessep473@gmail.com>
2018-10-06 16:19:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ function updateObjectWithKeyAndValue(obj, key, value) {
 const recipies = {eggs: 2 };
 
 updateObjectWithKeyAndValue(recipes, 'chocolate', '1 cup')
+
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
+  obj[key] = value
+ 
+  return obj
+}
+ 
+const recipe = { eggs: 3 }
+ 
+destructivelyUpdateObjectWithKeyAndValue(recipe, 'flour', '3 cups')
